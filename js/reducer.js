522e80666e03fcd31904e638e461e77c8218c9f2
Olha Gruzglina <olga.zhmura@gmail.com>
2022-05-18 14:54:19 -0400

    add state initialization as a default value to changeState  argument

--- js/reducer.js
+++ js/reducer.js
@@ -1,4 +1,7 @@
-function changeState(state, action){
+let state;// = {count: 0} // add this as a default value to changeState arguments
+let action = {type: 'INCREASE_COUNT'}
+
+function changeState(state = {count: 0}, action){
   switch (action.type) {
     case 'INCREASE_COUNT':
       return {count: state.count + 1}
@@ -7,7 +10,18 @@ function changeState(state, action){
   }
 }
 
-let state = {count: 0}
-let action = {type: 'INCREASE_COUNT'}
+function render() {
+  document.body.textContent = state.count;
+}
 
-changeState(state, action)
+function dispatch(action) {
+  state = changeState(state, action);
+  console.log('dispatch',state);
+  render();
+}
+dispatch({ type: "@@INIT" }); //will initialize state and show 0 in the page
+
+//or run in browser console dispatch({type: 'INCREASE_COUNT'})
+// or uncomment this - dispatch(action); but in this case you won't see 0
+
+
